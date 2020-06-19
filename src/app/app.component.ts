8e2c63cdbe1ba6d02c70e912c824f84f53e1dfb5
AKKA Germany GmbH <akka@AKKAs-MacBook-Pro.local>
2020-06-19 12:25:29 +0200

    COmmit to dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Third Pull Request app';
+  title = 'firstapp Change Again';
 }
